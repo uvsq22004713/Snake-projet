224012ce1b1783ae960fc01e97ba7401dbc2bc38
Hugo <hugo.nazac@ens.uvsq.fr>
2021-05-02 15:17:28 +0200

    carte add maj 2

--- snakeproject.py
+++ snakeproject.py
@@ -63,37 +63,18 @@ pseudo= tk.StringVar()
 ######################
 # Fonction
 
-def cartes():
+def cartes(taille):
 
     carte_une = []
-    for i in range(125):
+    for i in range(taille):
         temp = []
-        for j in range(50):
+        for j in range(taille):
             if i == 0 or i == 124 or j == 0 or j == 124:
                 temp.append(1)
             else:
                 temp.append(0)
         carte_une.append(temp)
 
-    carte_deux = []
-    for i in range(500):
-        temp = []
-        for j in range(500):
-            if i == 0 or i == 499 or j == 0 or j == 499:
-                temp.append(1)
-            else:
-                temp.append(0)
-        carte_deux.append(temp)
-
-    carte_trois = []
-    for i in range(350):
-        temp = []
-        for j in range(350):
-            if i == 0 or i == 349 or j == 0 or j == 349:
-                temp.append(1)
-            else:
-                temp.append(0)
-        carte_trois.append(temp)
 
 def reset():
     global score, pseudo, carte, objets, serpent, head_snake, direct
